b00aa0a1b40f49e3311eb6e7e5fc27444eb4dcc5
Piper Schwartz <piperschwartz@aol.com>
2022-08-09 12:34:08 -0600

    First commit

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+color = input("Enter a color: ")
+pluralNoun = input("Enter a plural noun:  ")
+celebrity = input("Enter a celebrity: ")
+
+print("Cupcakes are", color)
+print(pluralNoun + " are blue")
+print("I love", celebrity)
